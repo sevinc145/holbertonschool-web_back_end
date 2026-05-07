99a68a4f79df4fdc66a8b567aa47af6d864f42f1
root <sevinc.abdullayeva2008@gmail.com>
2026-05-07 15:05:47 +0000

    task

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' 8-all.py python_async_comprehension '''
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection.
+    """
+    return list(mongo_collection.find())
