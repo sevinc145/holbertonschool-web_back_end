79eb1e3f0191cde9c45b8d7aa34e6eb0825c0de0
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 10:26:50 +0000

    task

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id=None):
-        """ Creates Session ID for a user_id """
+        """ Creates Session ID """
 
         if user_id is None:
             return None
@@ -24,3 +24,14 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Returns a User ID based on a Session ID """
+
+        if session_id is None:
+            return None
+
+        if not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
