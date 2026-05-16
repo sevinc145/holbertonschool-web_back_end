b12d650352a6dc2a8d77bdceec850969a825fdc4
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 11:09:25 +0000

    task

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -44,8 +44,8 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
