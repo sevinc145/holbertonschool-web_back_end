e416b8a40da80a74811d34b53b9e384ac4777a16
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 09:31:24 +0000

    task

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic authentication module """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split('Basic ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decodes base64 authorization header """
+
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
