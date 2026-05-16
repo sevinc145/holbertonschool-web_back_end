cd09f462e3cf1efd6313fda14a26b6ee6f5943d3
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 09:42:11 +0000

    task

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,62 +1,26 @@
-#!/usr/bin/env python3
-""" Basic authentication module """
+def user_object_from_credentials(
+        self, user_email: str, user_pwd: str):
+    """ Returns User object from credentials """
 
-from api.v1.auth.auth import Auth
-import base64
+    if user_email is None or not isinstance(user_email, str):
+        return None
 
+    if user_pwd is None or not isinstance(user_pwd, str):
+        return None
 
-class BasicAuth(Auth):
-    """ BasicAuth class """
+    from models.user import User
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        """ Extracts Base64 authorization header """
+    try:
+        users = User.search({"email": user_email})
+    except Exception:
+        return None
 
-        if authorization_header is None:
-            return None
+    if len(users) == 0:
+        return None
 
-        if not isinstance(authorization_header, str):
-            return None
+    user = users[0]
 
-        if not authorization_header.startswith('Basic '):
-            return None
+    if not user.is_valid_password(user_pwd):
+        return None
 
-        return authorization_header.split('Basic ')[1]
-
-    def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        """ Decodes Base64 authorization header """
-
-        if base64_authorization_header is None:
-            return None
-
-        if not isinstance(base64_authorization_header, str):
-            return None
-
-        try:
-            decoded = base64.b64decode(
-                base64_authorization_header
-            )
-            return decoded.decode('utf-8')
-        except Exception:
-            return None
-
-    def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
-        """ Extracts user credentials """
-
-        if decoded_base64_authorization_header is None:
-            return (None, None)
-
-        if not isinstance(
-                decoded_base64_authorization_header, str):
-            return (None, None)
-
-        if ':' not in decoded_base64_authorization_header:
-            return (None, None)
-
-        user_email, user_pwd = (
-            decoded_base64_authorization_header.split(':', 1)
-        )
-
-        return (user_email, user_pwd)
+    return user
