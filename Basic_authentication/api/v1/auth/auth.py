1b803c9aed3c3b89495c6566005c517862609b4f
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 09:03:52 +0000

    task

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,23 @@ from typing import List, TypeVar
 class Auth:
     """ Auth class """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False """
-        return False
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """ checks if path requires auth """
+
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None """
