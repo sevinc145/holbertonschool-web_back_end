36e2900d2be1f73bfc947595d63aec005068fdc7
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-05-16 10:45:44 +0000

    task

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Session authentication views """
+
+from os import getenv
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+
+
+@app_views.route(
+    '/auth_session/login',
+    methods=['POST'],
+    strict_slashes=False
+)
+def login():
+    """ Login route """
+
+    email = request.form.get('email')
+
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+
+    if len(users) == 0:
+        return jsonify({
+            "error": "no user found for this email"
+        }), 404
+
+    user = users[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+
+    response.set_cookie(
+        getenv("SESSION_NAME"),
+        session_id
+    )
+
+    return response
