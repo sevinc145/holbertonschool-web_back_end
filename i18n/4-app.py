6a2e936056d2edd14aa21a1b8634f18695e46ce0
sevinc145 <sevinc.abdullayeva2008@gmail.com>
2026-05-30 22:46:48 +0400

    Update 4-app.py
    Updated string quotes from double to single in Babel configuration and locale handling.

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
-"""
-Basic Flask app with Babel localization
-"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """Babel configuration"""
-    LANGUAGES = ["en", "fr"]
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-babel = Babel(app)
-
 
 def get_locale():
-    """
-    Returns the locale from URL parameter if supported,
-    otherwise uses the best match from the request headers.
-    """
     locale = request.args.get('locale')
 
     if locale and locale in app.config['LANGUAGES']:
@@ -38,7 +29,6 @@ babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
-    """Render index page"""
     return render_template('4-index.html')
 
 
